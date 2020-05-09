245e2544480ea18f751d3ed91babf1a97313cf43
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-09 19:11:45 +0300

    Add files via upload

---
+++ BasicStreamClasses.java
@@ -0,0 +1,12 @@
+package javatraining.oletsky.io;
+
+/**
+ * @author O.Oletsky
+ */
+public class BasicStreamClasses {
+    public static void main(String[] args) {
+        System.out.println(System.in.getClass().getSimpleName());
+        System.out.println(System.out.getClass().getSimpleName());
+        System.out.println(System.err.getClass().getSimpleName());
+    }
+}
